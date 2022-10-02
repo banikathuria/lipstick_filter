d2c3c3497453790d2691df2a51c94af059689539
banikathuria <79353266+banikathuria@users.noreply.github.com>
2022-10-02 12:56:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 hairofsetx=""
 hairofsety=""
-clown_hair= ""
+
 noseofsetx=""
 noseofsety=""
-clown_nose=""
+
 nosex=""
 nosey=""
 function preload(){
-clown_nose=loadImage("clown_nose_img.jpg")
-clown_hair=loadImage("clown_hair_img.jpg")
+
 }
 function setup(){
 canvas= createCanvas(500,500)
@@ -21,8 +20,7 @@ posenet.on("pose",gotPoses)
 }
 function draw(){
 image(video,0,0,500,500)
-image(clown_nose,noseofsetx,noseofsety,40,40)
-image(clown_hair,hairofsetx,hairofsety,150,150)
+
 //fill("red")//
 //circle(nosex,nosey,40)//
 }
@@ -34,12 +32,8 @@ image(clown_hair,hairofsetx,hairofsety,150,150)
     {console.log(result)
         nosex=result[0].pose.nose.x
         nosey=result[0].pose.nose.y
-        noseofsetx=nosex-20
-        noseofsety=nosey-20
-        hairofsetx=nosex-20
-        hairofsety=nosey-150
-    }
-
+       
+       
  }
  
 
